6726202d409f51ffbec74b55ab0d3446fc78af7e
narellageorg <130798547+narellageorg@users.noreply.github.com>
2023-05-28 19:43:50 -0300

    Add files via upload

---
+++ js.js
@@ -0,0 +1,9 @@
+let nombre;
+
+nombre = prompt("Introduce tu nombre");
+
+if (nombre != null){
+	alert("Bienvenido/a, " + nombre + ".");
+} else {
+	alert("No has introducido nada. Recarga la página para intentarlo de nuevo.");
+}
